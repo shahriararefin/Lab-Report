a84697eab09be2e56a9177c68c08850dbefc2fdc
shahriararefin <shahriararefin7@gmail.com>
2022-06-18 17:10:17 +0600

    First Push

---
+++ calculatingtheaverage.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+
+    float num1=5.5,num2=3.6, num3=2.8, average, sum;
+
+    sum= num1+num2+num3;
+
+    average= sum/3;
+    printf("Average of 3 float number is: %.2f", average);
+
+
+
+    return 0;
+}
