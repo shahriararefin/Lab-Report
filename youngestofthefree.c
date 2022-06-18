6d09037481c3e5e98efcf68dfbf4a347e3a822c1
shahriararefin <shahriararefin7@gmail.com>
2022-06-18 19:27:41 +0600

    First Push

---
+++ youngestofthefree.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+int Arif, Fahmid, Joy;
+printf("Enter 3 Ages: \n");
+scanf("%d %d %d", &Arif, &Fahmid, &Joy);
+if (Arif<Fahmid && Arif<Joy){
+    printf("Arif is with the lowest age: %d\n ",Arif);
+}
+else if(Fahmid<Joy){
+    printf("Fahmid has the lowest age: %d\n", Fahmid);
+}
+
+else
+    {
+printf("Joy has the lowest age: %d", Joy);
+}
+return 0;
+} 
