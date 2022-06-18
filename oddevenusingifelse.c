5d903e0378c450685b0d8bbad7abbc43efed41f8
shahriararefin <shahriararefin7@gmail.com>
2022-06-18 19:48:27 +0600

    First Push

---
+++ oddevenusingifelse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+int num1,odd, even;
+printf("Enter a number: \n");
+scanf("%d", &num1);
+if (num1%2==0){
+    printf("Number is Even");
+}
+else{
+    printf("Number is Odd");
+}
+
+
+
+return 0;
+}
